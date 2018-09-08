e6d295b24716cace40021908ac4c135165669fb9
alexktech <42982558+alexktech@users.noreply.github.com>
2018-09-08 20:37:49 +0200

    feat(laureates): add basic table showing name, nationality, gender and prizes won for each laureate

--- src/app/laureates/laureates.component.ts
+++ src/app/laureates/laureates.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Laureate } from '../models/laureate.model';
+import { LaureateHttpService } from './laureate-http.service';
+
+
 @Component({
   selector: 'app-laureates',
   templateUrl: './laureates.component.html',
   styleUrls: ['./laureates.component.css']
 })
 export class LaureatesComponent implements OnInit {
+  columnsToDisplay = ['name', 'nationality', 'gender', 'prizes'];
 
-  constructor() { }
+  laureates: Laureate[] = [];
+
+  constructor(private laureateHttp: LaureateHttpService) {  }
 
   ngOnInit() {
+    this.laureateHttp.get().subscribe(response => this.laureates = response);
   }
 
 }
